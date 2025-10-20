df5c8461f15d4d9f670b3985805e89a36b36d795
Abubakar Mi <abubakarmi131@gmail.com>
2025-10-20 12:53:06 +0100

    git

--- Nyra.Domain/obj/Debug/net8.0/Nyra.Domain.AssemblyInfo.cs
+++ Nyra.Domain/obj/Debug/net8.0/Nyra.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nyra.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78669853ff96e1df2ffd7c778e9101c03735878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6640f643db4de96f798224a25aeb3bacce268c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nyra.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nyra.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
